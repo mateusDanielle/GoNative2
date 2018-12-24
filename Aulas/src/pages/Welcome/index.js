7156bf1286e31f4d269e65e3cec06ef6eaaa25ce
Mateus Danielle <mateusdanielle@edu.fecap.br>
2018-12-24 13:19:41 -0200

    Consultando API

--- Aulas/src/pages/Welcome/index.js
+++ Aulas/src/pages/Welcome/index.js
@@ -1,31 +1,66 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import {
-  View, Text, TextInput, TouchableOpacity, StatusBar,
+  View, Text, TextInput, TouchableOpacity, StatusBar, AsyncStorage,
 } from 'react-native';
 
 import styles from './styles';
 
-const Welcome = () => (
-  <View style={styles.container}>
-    <StatusBar barStyle="light-content" />
-    <Text style={styles.title}>Welcome</Text>
-    <Text style={styles.text}>
-      Para continuar precisamos que você informe seu usuário do GitHub
-    </Text>
-    <View style={styles.form}>
-      <TextInput
-        style={styles.input}
-        autoCapitalize="none"
-        autoCorrect={false}
-        placeholder="Digite seu usuário"
-        underlineColorAndroid="transparent"
-      />
-      <TouchableOpacity style={styles.button} onPress={() => {}}>
-        <Text style={styles.buttonText}>Prosseguir</Text>
-      </TouchableOpacity>
-    </View>
-  </View>
-);
+import api from '~/services/api';
 
-export default Welcome;
+export default class Welcome extends Component {
+  state = {
+    username: '',
+  };
+
+  checkUserExists = async (username) => {
+    const user = await api.get(`/users/${username}`);
+
+    return user;
+  };
+
+  saveUser = async (username) => {
+    await AsyncStorage.setItem('@Githuber:username', username);
+  };
+
+  singIn = async () => {
+    const { username } = this.state;
+    const { navigation } = this.props;
+
+    try {
+      await this.checkUserExists(username);
+      await this.saveUser(username);
+      navigation.navigate('Repositories');
+    } catch (err) {
+      console.tron.log('Usuário não existe');
+    }
+  };
+
+  render() {
+    const { username } = this.state;
+
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle="light-content" />
+        <Text style={styles.title}>Welcome</Text>
+        <Text style={styles.text}>
+          Para continuar precisamos que você informe seu usuário do GitHub
+        </Text>
+        <View style={styles.form}>
+          <TextInput
+            style={styles.input}
+            autoCapitalize="none"
+            autoCorrect={false}
+            placeholder="Digite seu usuário"
+            underlineColorAndroid="transparent"
+            value={username}
+            onChangeText={text => this.setState({ username: text })}
+          />
+          <TouchableOpacity style={styles.button} onPress={this.singIn}>
+            <Text style={styles.buttonText}>Prosseguir</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+}
