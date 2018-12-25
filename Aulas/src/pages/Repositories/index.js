06fe03065d4b328bee3da916cdfde288fbf53b8b
Mateus Danielle <mateusdanielle@edu.fecap.br>
2018-12-25 17:03:14 -0200

    Configurando o Header

--- Aulas/src/pages/Repositories/index.js
+++ Aulas/src/pages/Repositories/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 
 import { View, Text } from 'react-native';
 
+import Header from '~/components/Header';
 // import styles from './styles';
 
 const Repositories = () => (
   <View>
-    <Text>Teste</Text>
+    <Header title="Repositórios" />
   </View>
 );
 
